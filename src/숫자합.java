18d43ccf9554d352d6f00785840d4c74305a7328
Hajoon <dutls0124@naver.com>
2025-09-11 19:33:44 +0900

    숫자합 문제(11720번)
    다른 방법으로 풀어봤으나. RuntimeError/
    우선, 자연수의 자리수가 100개가 넘게 들어오는 값을 long으로 담아둘수가 없음. 심지어 double은 부동소수점 오차로 인해 굉장히 힘들어지는 상황

--- src/숫자합.java
+++ src/숫자합.java
@@ -6,13 +6,19 @@ public class 숫자합 {
     public static void main(String[] agrs) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long N = Long.parseLong(br.readLine());
-        String Numbers = br.readLine();
+        long Numbers = Long.parseLong(br.readLine());
 
         long sum = 0;
+        long number = Numbers;
 
-        char[] arr = Numbers.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            sum += Character.getNumericValue(arr[i]);
+        for (int i = 0; i < N; i++) {
+            double w = Math.pow(10,N-1-i);
+
+            long a =(long)((double) number % w);
+            long b = (long) ((double) number / w);
+
+            sum += b;
+            number = a;
         }
 
         System.out.println(sum);
