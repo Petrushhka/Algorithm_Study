e60fc3769b7284dae6da1f675eddf64c64f7ffbe
Hajoon <dutls0124@naver.com>
2025-09-11 18:38:59 +0900

    숫자합 문제(11720번)
    char -> long(int)로 변환
    Character.getNumericValue(X);

--- src/숫자합.java
+++ src/숫자합.java
@@ -3,20 +3,20 @@ import java.io.*;
 
 // https://www.acmicpc.net/problem/11720
 public class 숫자합 {
-public static void main(String[] agrs) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    long N = Long.parseLong(br.readLine());
-    String Numbers = br.readLine();
+    public static void main(String[] agrs) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        String Numbers = br.readLine();
 
-    long sum = 0;
+        long sum = 0;
 
-    char[] arr = Numbers.toCharArray();
-    for (int i = 0; i < arr.length; i++) {
-//        sum += Character.getNumericValue(arr[i]);
+        char[] arr = Numbers.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            sum += Character.getNumericValue(arr[i]);
+        }
+
+        System.out.println(sum);
+
+
     }
-
-    System.out.println(sum);
-
-
-}
 }
